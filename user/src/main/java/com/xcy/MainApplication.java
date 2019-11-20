ac3890641111b9c672c8fbbd4d1398aeb66c7ee5
komi213 <komi213@163.com>
2019-11-20 17:18:42 +0800

    完整model,dao,user实例
    完整model,dao,user实例

--- user/src/main/java/com/xcy/MainApplication.java
+++ user/src/main/java/com/xcy/MainApplication.java
@@ -4,10 +4,9 @@ import com.xcy.repository.XcyOrderRepository;
 import com.xcy.repository.XcyUserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = {XcyOrderRepository.class, XcyUserRepository.class})
 public class MainApplication {
